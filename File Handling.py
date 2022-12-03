427c75bc2e29b79fcab16d74682cf8491eb7288b
Anuj Doddakaragi <83464826+nimbus2009@users.noreply.github.com>
2022-12-03 20:29:22 +0530

    Added observer and sort on create

--- File Handling.py
+++ File Handling.py
@@ -1,10 +1,26 @@
+#Imports
 import shutil
+import sys
 import os
+import time
+import watchdog
 
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+#Variables
 def_from_dir="C:/Users/HP/Downloads"
 def_to_dir="C:/Users/HP/Documents"
 def_copy_dir="C:/Users/HP/Documents/Sorted"
 
+dir_tree = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.xlsx' '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+
+#Input variables
 from_dir=str(input("Enter the directory of the files to be sorted-"))
 if(from_dir==''):
     from_dir=def_from_dir
@@ -17,66 +33,62 @@ copy_dir=str(input("Enter the destination of the files to be sorted again-"))
 if(copy_dir==''):
     copy_dir=def_copy_dir
 
-list_of_files=os.listdir(from_dir)
 
 if(os.path.exists(copy_dir)==False):
     os.mkdir(copy_dir)
 
-for i in list_of_files:
-    name,ext=os.path.splitext(i)
-    print(name,ext)
-    
-    if(ext==''):
-        continue
-        #-----
-    elif(ext=='.png' or ext=='.jpg' or ext=='.gif' or ext=='.jfif' or ext=='.bmp' or ext=='.jpeg'):
-        tcd="C:/Users/HP/Documents/Sorted/image_files"
+path=from_dir
+dest=copy_dir
 
-        if(os.path.exists(tcd)==False):
-            os.mkdir(tcd)
+list_of_files=os.listdir(from_dir)
 
-        imageToBeCopied=from_dir+'/'+str(i)
-        shutil.copy(imageToBeCopied,tcd)
-        #-----
-    elif(ext=='.ppt' or ext=='.xls' or ext=='.xlsx' or ext=='.csv' or ext=='.pdf' or ext=='.txt'):
-        tcd="C:/Users/HP/Documents/Sorted/office_files"
+class FileMovementHandler(FileSystemEventHandler):
+    def on_created(self,event):
+        name,ext=os.path.splitext(event.src_path)
 
-        if(os.path.exists(tcd)==False):
-            os.mkdir(tcd)
-        
-        fileToBeCopied=from_dir+'/'+str(i)
-        shutil.copy(fileToBeCopied,tcd)
-        #----- 
-        
-    elif(ext=='.exe' or ext=='.bin' or ext=='.cmd' or ext=='.msi' or ext=='.dmg'):
-        tcd="C:/Users/HP/Documents/Sorted/setup_files"
+        for i in dir_tree:
+            for j in dir_tree[i]:
+                if(ext==j):
+                    file_name=os.path.basename(event.src_path)
+                    #Prints the tail part of file path
+                    print(file_name)
 
-        if(os.path.exists(tcd)==False):
-            os.mkdir(tcd)
-        
-        fileToBeCopied=from_dir+'/'+str(i)
-        shutil.copy(fileToBeCopied,tcd)
-        #-----
+                    key = i
 
-    elif(ext=='.mpg' or ext=='.mp2' or ext=='.mpeg' or ext=='.mpe' or ext=='.mpv' or ext=='.mp4' or ext=='.avi' or ext=='.mov'):
-        tcd="C:/Users/HP/Documents/Sorted/video_files"
+                    print("Downloaded " + file_name)
+                    path1 = path + '/' + file_name
+                    path2 = dest + '/' + key
+                    path3 = dest + '/' + key + '/' + file_name
 
-        if(os.path.exists(tcd)==False):
-            os.mkdir(tcd)
-        
-        videoToBeCopied=from_dir+'/'+str(i)
-        shutil.copy(videoToBeCopied,tcd)
-        
-    elif():
-        if(ext=='' or ext==' '):
-            continue
-        else:
-            tcd="C:/Users/HP/Documents/Sorted/other_files"
+                    time.sleep(3)
 
-            if(os.path.exists(tcd)==False):
-                os.mkdir(tcd)
-            
-            fileToBeCopied=from_dir+'/'+str(i)
-            shutil.copy(fileToBeCopied,tcd)
+                    if os.path.exists(path2):
+                        print("Directory Exists...")
+                        print("Moving " + file_name + "....")
+                        shutil.move(path1, path3)
+                        time.sleep(1)
+                    else:
+                        print("Making Directory...")
+                        os.makedirs(path2)
+                        print("Moving " + file_name + "....")
+                        shutil.move(path1, path3)
+                        time.sleep(1)
 
-            continue
+                    break
+
+
+#Initialise the event handler class
+event_handler=FileMovementHandler()
+
+#Initialise observer class
+observer=Observer()
+
+#Schedule the observer
+#The schedule method monitors the specified path which triggers any event to call event_handler
+observer.schedule(event_handler,path,recursive=True)
+#Start the observer
+observer.start()
+
+while True:
+    time.sleep(2)
+    print("Running...")
